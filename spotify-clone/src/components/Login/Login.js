667c4503cd6c435b6ab86f596ba4bfc3c8d2276b
Bobby Reay <rreay724@gmail.com>
2020-12-29 16:18:33 -0500

    intitial commit

---
+++ spotify-clone/src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://yungcitizen.com/wp-content/uploads/2018/09/spotify-logo-horizontal-white.png"
+        alt="logo"
+      />
+
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
