33a41b9ffb8c0195413b2fcf7ef33a6e561b4b47
Bobby Reay <rreay724@gmail.com>
2021-01-29 12:09:20 -0500

    intial amazon commit

---
+++ amazon-clone/src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <div className="homeContainer">
+        <img
+          className="homeImage"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+        <div className="homeRow"></div>
+        <div className="homeRow"></div>
+        <div className="homeRow"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "clone-2d1c5.firebaseapp.com",
+  projectId: "clone-2d1c5",
+  storageBucket: "clone-2d1c5.appspot.com",
+  messagingSenderId: "410859183865",
+  appId: "1:410859183865:web:7e90291de1adfadbf38c28",
+  measurementId: "G-DJENSCDEM7",
+};
