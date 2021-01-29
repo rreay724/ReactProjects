adcfb89ae3b074cbbc65c8811489fd0ff3a1a123
Bobby Reay <rreay724@gmail.com>
2021-01-29 11:51:33 -0500

    amazon clone

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header, Home } from "./components/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
