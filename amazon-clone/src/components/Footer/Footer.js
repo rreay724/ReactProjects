5c1395cf0a454b70f3c5b6697e9bf01c5bb6e9f9
Bobby Reay <rreay724@gmail.com>
2021-02-15 15:31:38 -0500

    footer styling

--- amazon-clone/src/components/Footer/Footer.js
+++ amazon-clone/src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./footer.css";
 
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
   return (
     <footer className="footer">
-      <div className="footerBackToTop">Back to top</div>
+      <div className="footerBackToTop">
+        <a onClick={scrollToTop}>Back to top</a>
+      </div>
       <div className="footerColumnGroup">
         <div className="footerColumn">
           <h3>Get to Know Us</h3>
@@ -28,24 +36,27 @@ function Footer() {
           <p>›See More Make Money with Us</p>
         </div>
         <div className="footerColumn">
-          <h3>Get to Know Us</h3>
-          <p>Careers</p>
-          <p>Blog</p>
-          <p>About Amazon</p>
-          <p>Press Center</p>
-          <p>Investor Relations</p>
-          <p>Amazon Devices</p>
-          <p>See More Make Money with Us</p>
+          <h3>Amazon Payment Products</h3>
+          <p>Amazon Rewards Visa Signature Cards</p>
+          <p>Amazon.com Store Card</p>
+          <p>Amazon Business Card</p>
+          <p>Amazon Business Line of Credit</p>
+          <p>Shop with Points</p>
+          <p>Credit Card Marketplace</p>
+          <p>Reload Your Balance</p>
+          <p>Amazon Currency Converter</p>
         </div>
         <div className="footerColumn">
-          <h3>Get to Know Us</h3>
-          <p>Careers</p>
-          <p>Blog</p>
-          <p>About Amazon</p>
-          <p>Press Center</p>
-          <p>Investor Relations</p>
-          <p>Amazon Devices</p>
-          <p>Amazon Tours</p>
+          <h3>Let Us Help You</h3>
+          <p>Amazon and COVID-19</p>
+          <p>Your Account</p>
+          <p>Your Orders</p>
+          <p>Shipping Rates & Policies</p>
+          <p>Amazon Prime</p>
+          <p>Returns & Replacements</p>
+          <p>Manage Your Content and Devices</p>
+          <p>Amazon Assistant</p>
+          <p>Help</p>
         </div>
       </div>
       <div className="footerBottom">
