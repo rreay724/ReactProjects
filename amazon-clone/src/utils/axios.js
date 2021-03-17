af3e61398da18dd08dba6e72b6fb4a7675d800f0
Bobby Reay <rreay724@gmail.com>
2021-03-17 11:53:00 -0400

    updated axios api link

--- amazon-clone/src/utils/axios.js
+++ amazon-clone/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-2d1c5/us-central1/api",
+  // got api link from functions in firebase console
+  baseURL: "https://us-central1-clone-2d1c5.cloudfunctions.net/api",
+  // baseURL: "http://localhost:5001/clone-2d1c5/us-central1/api",
 });
 
 export default instance;
